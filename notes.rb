4bccc5714bf6480f389d65ac45f68c6650731264
Lewis Jones <lewis.jones1997@protonmail.com>
2021-10-04 13:16:34 +0100

    update menu options

--- notes.rb
+++ notes.rb
@@ -4,17 +4,17 @@ class Notes
   def initialize
     @server = TCPServer.new(2345)
     @notes = []
-    start_server
   end
 
-  def start_server
+  def main
     while true
       Thread.start(@server.accept) do |s|
         puts("Something has connected")
         s.puts("1. Enter note\n2. View notes\n0. Exit")
         while input = s.gets.chomp
           break if input == "0"
-          s.puts("1. Enter note\n2. View notes")
+          s.puts "Please enter a correct option" if input != "1" || input != "2"
+          s.puts("1. Enter note\n2. View notes\n0. Exit")
           s.puts(input)
         end
         s.close
@@ -24,4 +24,5 @@ class Notes
   end
 end
 
-Notes.new
+app = Notes.new
+app.main
